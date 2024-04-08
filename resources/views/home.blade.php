758a0e0dd5d3cac0af5abbdf5a3730d52f883012
post-coder <gabrielspanu.dev@gmail.com>
2024-04-08 09:41:12 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel - Primi passi</title>
+</head>
+<body>
+    <h1>
+        Ciao a tutti!
+    </h1>
+</body>
+</html>
